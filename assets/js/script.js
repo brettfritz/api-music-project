6e4cf7bc05a82c33f5b2169b80a7677f59eb6213
brettfritz <brett.fritz3@gmail.com>
2024-04-11 21:18:08 -0500

    fixed syntax errors on js file and resolved console errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,12 @@ function fetchAccessToken() {
             accessToken = data.access_token;
             console.log('Access Token: ', accessToken)
         })
-        // console log the access token??
-        .then((data) => console.log(data));
 }
 
 
 function search() {
+
+    fetchAccessToken();
     // grabs input from search bar/form
     const query = $('#searchInput').val();
     // if there is no access token, stop
@@ -72,15 +72,15 @@ $(document).ready(() => {
   const openModal = $('#openModal');
   const closeModal = $('#closeModal');
 
-  $openModal.on('click', () => {
+  openModal.on('click', () => {
       modal.addClass('is-active');
   });
 
-  $closeModal.on('click', () => {
+  closeModal.on('click', () => {
       modal.removeClass('is-active');
   });
 
-  $modal.on('click', event => {
+  modal.on('click', event => {
       if ($(event.target).hasClass('modal-background') || $(event.target).hasClass('modal-close')) {
           modal.removeClass('is-active');
       }
@@ -117,14 +117,14 @@ $(document).ready(function () {
   });
   
   
-  const apiKey = 'Ftz45OlejK7c1TotYb8ypOJFkUrrbJzF';
-  fetchMusicNotesGIF(apiKey);
+  const apiKeyMusicNote = 'Ftz45OlejK7c1TotYb8ypOJFkUrrbJzF';
+  fetchMusicNotesGIF(apiKeyMusicNote);
   
-  function fetchMusicNotesGIF(apiKey) {
+  function fetchMusicNotesGIF(apiKeyMusicNote) {
     const query = 'music notes';
-    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`;
+    const apiUrlMusicNote = `https://api.giphy.com/v1/gifs/search?api_key=${apiKeyMusicNote}&q=${query}`;
   
-    fetch(apiUrl)
+    fetch(apiUrlMusicNote)
         .then(response => response.json())
         .then(data => {
             const gifUrl = data.data[0].images.original.url;
@@ -147,31 +147,4 @@ $(document).ready(function () {
     img.alt = 'Music Notes GIF';
     musicNotesDiv.appendChild(img);
   }
-  
-  
-  
-  // spotify client id: dcb4fb0557f74ae289fb0ebaab01d07d
-  
-  // spotify client secret: 8fb7747e40784aa890919eac0f2969c8
-  
-  // // http GET https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n \
-  // //   Authorization:'Bearer 1POdFZRZbvb...qqillRxMr2z'
-  
-  
-  //   curl --request GET \
-  //   'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V' \
-  //    --header "Authorization: Bearer NgCXRK...MzYjw"
-  
-  // The following code implements the getProfile() function which performs the API call to the Get Current User's Profile endpoint to retrieve the user profile related information:
-  
-  // async function getProfile(accessToken) {
-  // let accessToken = localStorage.getItem('access_token');
-  
-  // const response = await fetch('https://api.spotify.com/v1/me', {
-  //   headers: {
-  //     Authorization: 'Bearer ' + accessToken
-  //   }
-  // });
-  
-  // const data = await response.json();
   
