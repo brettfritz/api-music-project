8f1144f5be025edd58a150bf27d14a395e178f1d
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-04-11 21:20:27 -0500

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const clientId = "dcb4fb0557f74ae289fb0ebaab01d07d";
+let clientId = "dcb4fb0557f74ae289fb0ebaab01d07d";
 const clientSecret = "8fb7747e40784aa890919eac0f2969c8";
 const redirectURI = "";
 const code = undefined;
@@ -147,31 +147,4 @@ $(document).ready(function () {
     img.alt = 'Music Notes GIF';
     musicNotesDiv.appendChild(img);
   }
-  
-  
-  
-  // spotify client id: dcb4fb0557f74ae289fb0ebaab01d07d
-  
-  // spotify client secret: 8fb7747e40784aa890919eac0f2969c8
-  
-  // // http GET https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n \
-  // //   Authorization:'Bearer 1POdFZRZbvb...qqillRxMr2z'
-  
-  
-  //   curl --request GET \
-  //   'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V' \
-  //    --header "Authorization: Bearer NgCXRK...MzYjw"
-  
-  // The following code implements the getProfile() function which performs the API call to the Get Current User's Profile endpoint to retrieve the user profile related information:
-  
-  // async function getProfile(accessToken) {
-  // let accessToken = localStorage.getItem('access_token');
-  
-  // const response = await fetch('https://api.spotify.com/v1/me', {
-  //   headers: {
-  //     Authorization: 'Bearer ' + accessToken
-  //   }
-  // });
-  
-  // const data = await response.json();
   
